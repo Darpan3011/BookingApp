77bb975df1444cecf182bd7961f459afb528b72d
Darpan3011 <darpankanani301@gmail.com>
2023-09-20 09:03:55 +0530

    login

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import "./navbar.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+
 const Navbar = () => {
   const { user } = useContext(AuthContext);
 
@@ -12,10 +13,16 @@ const Navbar = () => {
           <span className="logo">Darpan's BookingApp</span>
         </Link>
         {/* <button className="logout" onClick={deleteCookie}>Logout</button> */}
-        {user ? user.username : (
+        {user ? user.username : ( // Hide navItems if user is logged in
           <div className="navItems">
-            <button className="navButton">Register</button>
-            <button className="navButton">Login</button>
+            {/* Link to Register */}
+            <Link to="/register" style={{ textDecoration: "none" }}>
+              <button className="navButton">Register</button>
+            </Link>
+            {/* Link to Login */}
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <button className="navButton">Login</button>
+            </Link>
           </div>
         )}
       </div>
