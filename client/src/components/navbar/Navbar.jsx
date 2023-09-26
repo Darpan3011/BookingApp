364bac26b6a77eb2abef0fe6c3417ec25ef7dc58
Darpan3011 <darpankanani301@gmail.com>
2023-09-26 12:59:07 +0530

    added logout

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -6,14 +6,26 @@ import { AuthContext } from "../../context/AuthContext";
 const Navbar = () => {
   const { user } = useContext(AuthContext);
 
+  // Function to handle logout
+  const handleLogout = () => {
+    // Remove the user from localStorage
+    localStorage.removeItem('user');
+    window.location.reload();
+    // You can also perform any additional logout logic here (e.g., clearing the user context)
+  };
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">Darpan's BookingApp</span>
         </Link>
-        {/* <button className="logout" onClick={deleteCookie}>Logout</button> */}
-        {user ? user.username : ( // Hide navItems if user is logged in
+        {user ? (
+          <div className="userContainer">
+            <span className="username">{user.username}</span>
+            <button className="navButton" onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
           <div className="navItems">
             {/* Link to Register */}
             <Link to="/register" style={{ textDecoration: "none" }}>
